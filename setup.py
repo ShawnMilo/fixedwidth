b77321d909eeb4fa3b233a13131ef847ebdb8c68
Shawn Milochik <shawn@milochik.com>
2012-12-08 16:24:39 -0500

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from distutils.core import setup
 
 setup(
-    name = 'FixedWidth',
-    version = '0.1',
-    packages = ['fixedwidth',],
-    license = 'BSD',
+    name='FixedWidth',
+    version='0.11',
+    description='Two-way fixed-width / Python dict converter.',
+    packages=['fixedwidth',],
+    license='BSD',
+    url='https://github.com/ShawnMilo/fixedwidth',
     author='Shawn Milochik',
     author_email='shawn@milochik.com',
+    zip_safe=False,
     
 )
