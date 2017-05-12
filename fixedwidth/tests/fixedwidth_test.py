d4ea36090622fafdf3f5bbff6a34965bf9d5ab5d
Dave Caraway <dave@fogmine.com>
2017-05-12 17:57:14 +0000

    attempt to convert default string values to proper type when mismatched

--- fixedwidth/tests/fixedwidth_test.py
+++ fixedwidth/tests/fixedwidth_test.py
@@ -60,8 +60,9 @@ SAMPLE_CONFIG = {
     },
 
     "latitude": {
-        "required": True,
+        "required": False,
         "type": "decimal",
+        "default": "52.23",
         "start_pos": 69,
         "end_pos": 78,
         "alignment": "right",
@@ -69,7 +70,8 @@ SAMPLE_CONFIG = {
         },
 
     "longitude": {
-        "required": True,
+        "required": False,
+        "default": Decimal('4.38'),
         "type": "decimal",
         "start_pos": 79,
         "end_pos": 89,
@@ -78,8 +80,9 @@ SAMPLE_CONFIG = {
         },
 
     "elevation": {
-        "required": True,
+        "required": False,
         "type": "integer",
+        "default": 7,
         "start_pos": 90,
         "end_pos": 93,
         "alignment": "right",
@@ -114,7 +117,7 @@ class TestFixedWidth(unittest.TestCase):
             "032vegetarian             40.7128   -74.0059-100\r\n"
         )
 
-        self.assertEquals(fw_string, good)
+        self.assertEqual(fw_string, good)
 
     def test_update(self):
         """
@@ -132,10 +135,10 @@ class TestFixedWidth(unittest.TestCase):
 
         #change a value
         fw_obj.update(meal="Paleo")
-        self.assertEquals(fw_obj.data["meal"], "Paleo")
+        self.assertEqual(fw_obj.data["meal"], "Paleo")
 
         #nothing else should have changed
-        self.assertEquals(fw_obj.data["first_name"], "Michael")
+        self.assertEqual(fw_obj.data["first_name"], "Michael")
 
     def test_fw_to_dict(self):
         """
@@ -151,10 +154,128 @@ class TestFixedWidth(unittest.TestCase):
         )
 
         values = fw_obj.data
-        self.assertEquals(values["first_name"], "Michael")
-        self.assertEquals(values["last_name"], "Smith")
-        self.assertEquals(values["age"], 32)
-        self.assertEquals(values["meal"], "vegetarian")
-        self.assertEquals(values["latitude"], Decimal('40.7128'))
-        self.assertEquals(values["longitude"], Decimal('-74.0059'))
-        self.assertEquals(values["elevation"], -100)
+        self.assertEqual(values["first_name"], "Michael")
+        self.assertEqual(values["last_name"], "Smith")
+        self.assertEqual(values["age"], 32)
+        self.assertEqual(values["meal"], "vegetarian")
+        self.assertEqual(values["latitude"], Decimal('40.7128'))
+        self.assertEqual(values["longitude"], Decimal('-74.0059'))
+        self.assertEqual(values["elevation"], -100)
+
+    def test_default_conversion_decimal(self):
+        """
+        Test that default value is converted when it doesn't match the type
+        """
+        config = {
+            "defaulttypemismatchstr": {
+                "required": False,
+                "type": "decimal",
+                "default": "52.23",
+                "start_pos": 1,
+                "length": 8,
+                "alignment": "right",
+                "padding": " "
+                },
+            }
+        good_line = "{:8}\r\n".format(52.23)
+        fw_obj = FixedWidth(config)
+
+        #defaulttypemismatch value not set so will use default value
+        self.assertEqual(good_line, fw_obj.line)
+        self.assertIsInstance(fw_obj.data["defaulttypemismatchstr"], Decimal)
+
+
+    def test_default_conversion_fails(self):
+        """
+        Test that value error is raised when default mismatch and conversion fails
+        """
+        field_name = "defaulttypemismatchbad"
+        field_type = "decimal"
+
+        config = {
+            field_name: {
+                "required": False,
+                "type": field_type,
+                "default": "bad",
+                "start_pos": 1,
+                "length": 8,
+                "alignment": "right",
+                "padding": " ",
+            },
+        }
+
+        self.assertRaisesRegex(ValueError, "^Unable to convert.*$", FixedWidth, config)
+
+    def test_default_conversion_integer(self):
+        """
+        Test that default integer value is converted when it doesn't match the type
+        """
+        field_name = "defaulttypemismatch"
+        field_type = "integer"
+
+        config = {
+            field_name: {
+                "required": False,
+                "type": field_type,
+                "default": "52",
+                "start_pos": 1,
+                "length": 8,
+                "alignment": "right",
+                "padding": " ",
+            },
+        }
+        good_line = "{:8}\r\n".format(52)
+        fw_obj = FixedWidth(config)
+
+        #field not set so will use default value
+        self.assertEqual(good_line, fw_obj.line)
+        self.assertIsInstance(fw_obj.data["defaulttypemismatch"], int)
+
+    def test_decimal_float_conversion(self):
+        """
+        Test that value error is raised when default mismatch, type is decimal and
+        the type of the default is a float.
+
+        Note:
+            Floats aren't supported for Decimal types since they aren't as
+            exact as Decimal for serialization/deserialization of fixed width documents.
+        """
+        field_name = "defaultstrmismatch"
+        field_type = "string"
+
+        config = {
+            field_name: {
+                "required": False,
+                "type": field_type,
+                "default": 52.23,
+                "start_pos": 1,
+                "length": 8,
+                "alignment": "right",
+                "padding": " ",
+            },
+        }
+
+        self.assertRaisesRegex(ValueError, "^Default value for %s "\
+                "is not a valid %s$" % (field_name, field_type), FixedWidth, config)
+
+    def test_str_int_conversion(self):
+        """
+        Test that value error is raised when default mismatch, type is not string.
+        """
+        field_name = "defaultstrmismatch"
+        field_type = "string"
+
+        config = {
+            field_name: {
+                "required": False,
+                "type": field_type,
+                "default": 52.23,
+                "start_pos": 1,
+                "length": 8,
+                "alignment": "right",
+                "padding": " ",
+            },
+        }
+
+        self.assertRaisesRegex(ValueError, "^Default value for %s "\
+                "is not a valid %s$" % (field_name, field_type), FixedWidth, config)
