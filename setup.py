650a9847648e5b26632cf8c03b35cee8724a0610
Shawn Milochik <shawn@milochik.com>
2017-05-13 14:58:29 -0400

    Update to 1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='FixedWidth',
     packages=['fixedwidth'],
-    version='0.99.7',
+    version='1.0',
     description='Two-way fixed-width <--> Python dict converter.',
     author='Shawn Milochik',
     author_email='shawn@milochik.com',
